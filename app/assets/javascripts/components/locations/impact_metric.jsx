399ae6483630b96814b1ecd53e1528aa6360565a
Jonah Price <jyoonah@gmail.com>
2017-09-19 13:02:46 -0700

    Remove unecessary refrences to state from impact metric componnent.

--- app/assets/javascripts/components/locations/impact_metric.jsx
+++ app/assets/javascripts/components/locations/impact_metric.jsx
@@ -1,19 +1,12 @@
 class ImpactMetric extends React.Component {
-  constructor(props){
-    super(props){
-      this.state = {
-        metricValue: 0,
-        metricUnit: "",
-        subText: "",
-      };
-    }
-  }
 
   render() {
-    <div className="impact-metric">
-      <span className="metric-value">{this.state.metricValue}</span>
-      <span className="metric-unit">{this.state.metricUnit}</span>
-      <span className="subtext">{this.state.subText}</span>
-    </div>
+    return (
+      <div className="impact-metric">
+        <span className="metric-value">{this.props.metricValue} </span>
+        <span className="metric-unit">{this.props.metricUnit} </span>
+        <span className="subtext">{this.props.subText} </span>
+      </div>
+    )
   }
 }
